524015c9b92f9af0fefda8c70200b19f31c1eb73
Martin.Kono <martin.konopasek@proton.me>
2023-08-24 20:19:52 -0400

    created flex-box structure and added margin/padding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 
 const App = () => {
   return (
-    <div>
+    <div className="div-app">
       <QRcode/>
     </div>
   )
--- src/components/QRcode.js
+++ src/components/QRcode.js
@@ -3,10 +3,10 @@ import image from "../images/image-qr-code.png"
 
 const QRcode = () => {
   return (
-    <div>
-        <img id="image-qrcode" src={image} alt=""/>
-        <h2>Improve your front-end skills by building projects</h2>
-        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    <div className="qr-component">
+        <img src={image} alt=""/>
+        <h2>Improve your front-end<br/>skills by building projects</h2>
+        <p>Scan the QR code to visit Frontend<br/> Mentor and take your coding skills to<br/> the next level</p>
     </div>
   )
 }
